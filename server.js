61f31fe714eedff5295477cff2f81e47c9f047ec
Uprosper <uchennamiracle89@gmail.com>
2026-06-20 21:26:14 +0100

    Update and rename src/server.js to server.js

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/contact', contactRoutes);
+app.use('/api/admin', adminSeedRoute);
 
 // --- 404 handler ---
 app.use((req, res) => {
