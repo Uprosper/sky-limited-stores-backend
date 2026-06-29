2202a7c980de64c6753707001eeb08253282178b
Uprosper <uchennamiracle89@gmail.com>
2026-06-29 16:47:56 +0100

    Update seed.js

--- src/seed.js
+++ src/seed.js
@@ -516,7 +516,7 @@ const products = [
 
 const seed = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
     await Product.deleteMany({});
     console.log('Cleared existing products');
